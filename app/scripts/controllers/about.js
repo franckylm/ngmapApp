f74d5f824b20fc2a680b9b1bc31f143e48f1918b
franckylm <franck.libam@gmail.com>
2014-07-01 23:50:44 +0200

    application scaffolding using yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngmapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngmapApp
+ */
+angular.module('ngmapApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
